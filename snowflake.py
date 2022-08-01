7255191ae452fa00db11e0c64aa8e8e41e6b3eb3
andrewjeffallen <andrewjeffallen@gmail.com>
2022-07-31 21:12:12 -0700

    comments

--- snowflake.py
+++ snowflake.py
@@ -7,16 +7,17 @@ import pandas as pd
 
 
 class SnowflakeConnection:
-    def __init__(self,
-                 user: str,
-                 password: str,
-                 account: str,
-                 warehouse: str,
-                 database: str,
-                 schema: str,
-                 role: str
-                ) -> None :
-        
+    def __init__(
+        self,
+        user: str,
+        password: str,
+        account: str,
+        warehouse: str,
+        database: str,
+        schema: str,
+        role: str,
+    ) -> None:
+
         self.user = user
         self.password = password
         self.account = account
@@ -24,41 +25,48 @@ class SnowflakeConnection:
         self.database = database
         self.schema = schema
         self.role = role
-    
+
     def connect(self):
         try:
             con = snowflake.connector.connect(
-                user = self.user, 
-                password = self.password ,
-                account = self.account ,
-                warehouse = self.warehouse,
-                database =self.database,
-                schema = self.schema ,
-                role= self.role 
+                user=self.user,
+                password=self.password,
+                account=self.account,
+                warehouse=self.warehouse,
+                database=self.database,
+                schema=self.schema,
+                role=self.role,
             )
             return con
         except Exception as e:
             raise e
-            
+
     def load_snowflake_json(self, table):
-        
+        """
+        :params: table
+            Alias table: surf_spot_alias
+            Logs table: surf_logs
+        """
+
         ctx = self.connect()
 
-        with open ('getsurf/surf_logs/Pipes_06_17_2022_03PM.json', 'r') as infile:
+        with open("getsurf/surf_logs/Pipes_06_17_2022_03PM.json", "r") as infile:
             data = json.load(infile)
 
         cs = ctx.cursor()
         try:
             cs.execute(f"use warehouse surfline_wh")
-            cs.execute(f"insert into surf_logs.{table} (select PARSE_JSON('%s'))" % json.dumps(data))
+            cs.execute(
+                f"insert into surf_logs.{table} (select PARSE_JSON('%s'))"
+                % json.dumps(data)
+            )
         finally:
             cs.close()
         ctx.close()
-        
+
     def query_snowflake_json(self):
 
         ctx = self.connect()
-
 
         cs = ctx.cursor()
         try:
@@ -70,19 +78,3 @@ class SnowflakeConnection:
         finally:
             cs.close()
             ctx.close()
-
-
-
-snow_object = SnowflakeConnection(
-            user='surfline_user',
-            password=get_secret('surfline_db_password').get('surfline_db_password'),
-            account='bka04153',
-            warehouse='surfline_wh',
-            database='surfline',
-            schema='surfline_logs',
-            role='surfline_role'
-        )
-
-df_raw = snow_object.query_snowflake_json()
-pd.json_normalize(json.loads(df_raw.SURF_LOGS[0]))
-
